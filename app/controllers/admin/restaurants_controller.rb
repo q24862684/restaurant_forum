dc5c289aeff8bc0cfcadae3b057e9a09421a3805
q24862684 <q24862684@hotmail.com>
2017-12-12 18:45:34 +0800

    add column image to restaurants table

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -44,6 +44,6 @@ class Admin::RestaurantsController < ApplicationController
 
   end
   def restaurant_params
-    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image)
   end
 end
