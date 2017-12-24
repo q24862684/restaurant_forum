1aafdf56e396fbe667dc57d9a41db2483e763d5c
q24862684 <q24862684@hotmail.com>
2017-12-24 23:14:32 +0800

    ser on index page can select restaurants by category

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,7 +4,4 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.page(params[:page]).per(9)
     @categories = Category.all
   end
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
 end
